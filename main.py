64ff2ae7ceecf91751b90757ff45227e11df8771
akhatamibellevue <107235052+akhatamibellevue@users.noreply.github.com>
2023-02-23 18:56:53 -0500

    Cleaned, scaled, and transformed the data

--- main.py
+++ main.py
@@ -9,3 +9,20 @@ from sklearn.preprocessing import StandardScaler
 # Read the data
 df = pd.read_csv("Airbnb Prices in Europe/amsterdam_weekdays.csv")
 df.drop(['Unnamed: 0'], axis=1, inplace=True)
+
+# Find categorical columns and numerical columns
+cat_cols = df.select_dtypes(include=['object', 'bool']).columns.tolist()
+num_cols = df.select_dtypes(include=['int64', 'float64']).columns.tolist()
+num_cols.remove('realSum')
+
+# Define the transformers for numerical and categorical columns
+num_transformer = Pipeline(steps=[('imputer', SimpleImputer(strategy='mean')), ('scaler', StandardScaler())])
+cat_transformer = Pipeline(steps=[('imputer', SimpleImputer(strategy='most_frequent')), ('onehot', OneHotEncoder(handle_unknown='ignore'))])
+
+# Bundle the transformers
+preprocessor = ColumnTransformer(transformers=[('num', num_transformer, num_cols), ('cat', cat_transformer, cat_cols)])
+
+# Apply the transformations to the data
+df = preprocessor.fit_transform(df)
+
+df = pd.DataFrame(df)
