21cb76339d0547bb2d9cbd4bdcc13aa02da5a4e8
akhatamibellevue <107235052+akhatamibellevue@users.noreply.github.com>
2023-02-23 22:01:45 -0500

    Splitted the data and transformed it

--- main.py
+++ main.py
@@ -10,10 +10,13 @@ from sklearn.preprocessing import StandardScaler
 df = pd.read_csv("Airbnb Prices in Europe/amsterdam_weekdays.csv")
 df.drop(['Unnamed: 0'], axis=1, inplace=True)
 
+# Split the data into features and target
+X = df.drop(['realSum'], axis=1)
+y = df['realSum']
+
 # Find categorical columns and numerical columns
-cat_cols = df.select_dtypes(include=['object', 'bool']).columns.tolist()
-num_cols = df.select_dtypes(include=['int64', 'float64']).columns.tolist()
-num_cols.remove('realSum')
+cat_cols = X.select_dtypes(include=['object', 'bool']).columns.tolist()
+num_cols = X.select_dtypes(include=['int64', 'float64']).columns.tolist()
 
 # Define the transformers for numerical and categorical columns
 num_transformer = Pipeline(steps=[('imputer', SimpleImputer(strategy='mean')), ('scaler', StandardScaler())])
@@ -22,16 +25,23 @@ cat_transformer = Pipeline(steps=[('imputer', SimpleImputer(strategy='most_frequ
 # Bundle the transformers
 preprocessor = ColumnTransformer(transformers=[('num', num_transformer, num_cols), ('cat', cat_transformer, cat_cols)])
 
+# _________________________________________________________________
+from sklearn.model_selection import train_test_split
+
+# Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Split the training data into training and validation sets
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
+
+
 # Apply the transformations to the data
-df = preprocessor.fit_transform(df)
+X_train_processed = preprocessor.fit_transform(X_train)
+X_val_processed = preprocessor.transform(X_val)
+X_test_processed = preprocessor.transform(X_test)
 
-df = pd.DataFrame(df)
 
-from scipy import stats
 
-# Calculate the z-score for each numeric feature
-z_scores = stats.zscore(df.select_dtypes(include=["int", "float"]))
 
-# Remove rows where any of the z-scores are greater than 3 or less than -3
-df = df[(z_scores < 3).all(axis=1) & (z_scores > -3).all(axis=1)]
+
 
