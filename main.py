43e343f97e9293350122c381156a8a4090848f32
akhatamibellevue <107235052+akhatamibellevue@users.noreply.github.com>
2023-02-24 01:02:50 -0500

    combined the data, split it into training, validation, and testing sets, and preprocessed the data using a pipeline that handles numerical and categorical data separately

--- main.py
+++ main.py
@@ -1,8 +1,55 @@
 import pandas as pd
 import glob
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+
 
 data_list = glob.glob('Airbnb Prices in Europe/*.csv')
 
 # Now we can use pandas to read all the csv files and combine them into one
 df = pd.concat(map(pd.read_csv, data_list))
 df.drop('Unnamed: 0', axis=1, inplace=True)
+
+# Set X and y
+X = df.drop('realSum', axis=1)
+y = df.realSum
+
+# Get numerical and categorical columns
+num_cols = [col for col in X.columns if X[col].dtype in ['int64', 'float64']]
+cat_cols = [col for col in X.columns if X[col].dtype == 'object']
+
+# Preprocessing for numerical data
+num_transformer = SimpleImputer(strategy='mean')
+scaler = StandardScaler()
+
+# Preprocessing for categorical data
+cat_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='most_frequent')),
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))
+])
+
+# Use ColumnTransformer to combine the preprocessing steps
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', num_transformer, num_cols),
+        ('cat', cat_transformer, cat_cols)
+    ])
+
+# Use the pipeline to process the data
+pipeline = Pipeline(steps=[('preprocessor', preprocessor),
+                            ('scaler', scaler)])
+X_processed = pipeline.fit_transform(X)
+
+# Split the data into training and validation sets
+X_train, X_valid, y_train, y_valid = train_test_split(X_processed, y, test_size=0.2, random_state=42)
+
+# Split the training data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
+
+
